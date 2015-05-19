5e478071fe087b933f99797520131170e29f5fa4
Zibo <zibo@gmail.com>
2015-05-19 19:05:21 +0200

    koniec pracy nad zmianami w funkcji dodawania

--- kalkulator.py
+++ kalkulator.py
@@ -1,10 +1,10 @@
-def dodawanie():
+def dodawanie(a, b):
 	return a+b
 print('Podaj pierwsza liczbe')
 a=int(input())
 print('Podaj druga liczbe')
 b=int(input())
-print(a+b)
 
-dodawanie()
+
+print(dodawanie(a, b))
 
